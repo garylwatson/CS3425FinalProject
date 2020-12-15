e1a3f5d98627eb1483755fb6505faf694377cf0c
Gary Watson <glwatson@colossus.it.mtu.edu>
2020-12-15 18:18:25 -0500

    logging in for teacher and student now works

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
 <?php 
 session_start();
+
+if(isset($_POST['login'])){
+	//can't login as two people at the same time
+	if($_POST['teachuser'] != '' and $_POST['stuid'] != ''){
+		echo 'You may only login as teacher or student, not both';
+	//if we are trying to login as teacher
+	} else if($_POST['teachuser'] != ''){
+		//if username and password for teacher match predefined account
+		if ($_POST['teachuser'] == 'user' and $_POST['teachpass'] == 'password'){
+			echo "You're logged in!";
+			sleep(1);
+			header("LOCATION:teacherlogin.php");
+		} else {
+			echo "Wrong username or password, please try again!";
+		}
+	//we have to dynamically check student login
+	} else if(isset($_POST['stuid'])){
+		$_SESSION['username'] = $_POST['stuid'];
+		$userDefinedPassword = $_POST['stupass'];
+
+		$config = parse_ini_file("db.ini");
+                $dbh = new PDO($config['dsn'], $config["username"], $config["password"]);
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		//get password from database
+		$stmt = $dbh->prepare("SELECT password FROM student WHERE id = :id");
+		$stmt->execute(['id'=>$_SESSION['username']]);
+
+		$databaseDefinedPassword = $stmt->fetchColumn();
+		
+		//if the user put in the right password
+		if($userDefinedPassword == $databaseDefinedPassword){
+			echo "You're logged in!";
+			sleep(1);
+			header("LOCATION:studentlogin.php");
+		} else {
+			echo "Student: Wrong username or password, please try again!";
+		}	
+	}
+}		
+		
  ?>
 <!DOCTYPE html>
 <html>
@@ -9,19 +50,16 @@ session_start();
 </head>
 
 <body>
-<h1> Teacher login </h1>
-<form form action="teacherlogin.php" method="post">
+<h3> Teacher login </h3>
+<form form action="index.php" method="post">
 User Name: <input type="text" name="teachuser">
 Password: <input type="password" name="teachpass">
-<input type="submit" name="login" value="Submit">
-</form>
 
-
-<h1>Student login </h1>
-<form form action="studentlogin.php" method="post">
+<h3>Student login </h3>
 Student ID: <input type="text" name="stuid">
 Password: <input type="password" name="stupass">
-<input type="submit" name="login" value="Submit">
+</br>
+<input type="submit" name="login" value="Login">
 </form>
 </body>
 </html>
